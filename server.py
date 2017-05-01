d03d43e443beb4975f8ddd0fa28cf362d3a04ab7
androstudio <androstudioteam@gmail.com>
2017-05-01 13:05:25 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,93 @@
+import os
+from flask import Flask, request
+from twilio.jwt.access_token import AccessToken, VoiceGrant
+from twilio.rest import Client
+import twilio.twiml
+
+ACCOUNT_SID = 'AC***'
+API_KEY = 'SK***'
+API_KEY_SECRET = '***'
+PUSH_CREDENTIAL_SID = 'CR***'
+APP_SID = 'AP***'
+
+IDENTITY = 'voice_test'
+CALLER_ID = 'quick_start'
+
+app = Flask(__name__)
+
+@app.route('/accessToken')
+def token():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  api_key = os.environ.get("API_KEY", API_KEY)
+  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
+  push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID", PUSH_CREDENTIAL_SID)
+  app_sid = os.environ.get("APP_SID", APP_SID)
+
+  grant = VoiceGrant(
+    push_credential_sid=push_credential_sid,
+    outgoing_application_sid=app_sid
+  )
+
+  token = AccessToken(account_sid, api_key, api_key_secret, IDENTITY)
+  token.add_grant(grant)
+
+  return str(token)
+
+@app.route('/outgoing', methods=['GET', 'POST'])
+def outgoing():
+  resp = twilio.twiml.Response()
+  resp.say("Congratulations! You have made your first oubound call! Good bye.")
+  return str(resp)
+
+@app.route('/incoming', methods=['GET', 'POST'])
+def incoming():
+  resp = twilio.twiml.Response()
+  resp.say("Congratulations! You have received your first inbound call! Good bye.")
+  return str(resp)
+
+@app.route('/placeCall', methods=['GET', 'POST'])
+def placeCall():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  api_key = os.environ.get("API_KEY", API_KEY)
+  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
+
+  client = Client(api_key, api_key_secret, account_sid)
+  call = client.calls.create(url=request.url_root + 'incoming', to='client:' + IDENTITY, from_='client:' + CALLER_ID)
+  return str(call.sid)
+
+@app.route('/call', methods=['GET', 'POST'])
+def call():
+  """ This method routes calls from/to client                  """
+  """ Rules: 1. From can be either client:name or PSTN number  """
+  """        2. To value specifies target. When call is coming """
+  """           from PSTN, To value is ignored and call is     """
+  """           routed to client named CLIENT                  """
+  resp = twilio.twiml.Response()
+  from_value = request.values.get('From')
+  to = request.values.get('To')
+  if not (from_value and to):
+    resp.say("Invalid request")
+    return str(resp)
+  from_client = from_value.startswith('client')
+  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+  if not from_client:
+    # PSTN -> client
+    resp.dial(callerId=from_value).client(CLIENT)
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
+  return str(resp)
+
+  
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+  resp = twilio.twiml.Response()
+  resp.say("Welcome to Twilio")
+  return str(resp)
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
